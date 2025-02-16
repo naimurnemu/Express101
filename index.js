63f57f20914f5883da2ad56c72e7a0645c8980c5
naimurnemu <naimurrahaman82@gmail.com>
2025-02-16 23:55:58 +0600

    file handle in express

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
+
+app.get("/", (req, res) => {
+  fs.readFile("./pages/index.html", (err, data) => {
+    if (err) {
+      res.send(`
+        <h2>Something went wrong</h2>
+      `);
+    } else {
+      res.write(data);
+      res.end();
+    }
+  })
+
+});
+
+app.get("/about", (req, res) => {
+  fs.readFile("./pages/about.html", (err, data) => {
+    if (err) {
+      res.send(`<h2>This is Something Wrong in About</h2>`)
+    } else {
+      res.write(data);
+      res.end();
+    }
+  })
+
+});
+
+app.get("/help", (req, res) => {
+  fs.readFile("./pages/help.html", (err, data) => {
+    if (err) {
+      res.send(`
+      <h2>This is an Error in help page</h2>
+    `);
+    } else {
+      res.write(data);
+      res.end();
+    }
+  })
+
+});
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 })
-
-app.get("/", (req, res) => {
-  res.send(`
-    <h2>This is Home Page</h2>
-  `);
-});
-
-app.get("/about", (req, res) => {
-  res.send(`
-    <h2>This is About Page</h2>
-  `);
-});
-
-app.get("/help", (req, res) => {
-  res.send(`
-    <h2>This is Help Page</h2>
-  `);
-});
